c8ce9022e12c721b44152cb92237922cc6923245
Vitali Mostowoj <vmostowoj@de.pepperl-fuchs.com>
2025-03-06 14:43:15 +0100

    Add prometheus endpoint and conter value
    Change-Id: Ifaca32f8376773a26f35d9d2832e69b36c7512e1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from prometheus_client import Gauge, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
 import os
 
 app = FastAPI()
 
+counter = int(os.getenv("COUNTER", 0))  # Get counter value from environment variable or default to 0
+
+# Create a Prometheus Gauge metric to reflect the counter value
+PROMETHEUS_COUNTER = Gauge("app_counter", "Counter value from FastAPI app")
+PROMETHEUS_COUNTER.set(counter)  # Initialize with the current counter value
+
+# Define a Prometheus Counter
 
 @app.get("/")
 async def root():
@@ -11,4 +20,18 @@ async def root():
     pod_namespace = os.getenv("MY_POD_NAMESPACE", "unknown")
     pod_ip = os.getenv("MY_POD_IP", "unknown")
 
-    return {"node_name": node_name, "pod_name": pod_name, "pod_namespace": pod_namespace, "pod_ip": pod_ip}
+    return {"node_name": node_name, "pod_name": pod_name, "pod_namespace": pod_namespace, "pod_ip": pod_ip, "error_couter": counter}
+
+#curl -X 'POST' 'http://127.0.0.1:8000/set_counter?value=10'
+
+@app.post("/set_counter")
+def set_counter(value: int):
+    global counter
+    counter = value
+    os.environ["COUNTER"] = str(counter)  # Update environment variable
+    PROMETHEUS_COUNTER.set(counter)  # Update Prometheus metric with new value
+    return {"message": "Counter updated", "counter": counter}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
